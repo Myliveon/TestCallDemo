c5bf7859a4eafd3518a16fad0b796b1603a9acef
mylive <myliveon@163.com>
2017-07-04 10:02:15 +0800

    init
    init

---
+++ ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  TestCallDemo
+//
+//  Created by 马瑜 on 2016/12/19.
+//  Copyright © 2016年 马瑜. All rights reserved.
+//
+
+import UIKit
+import CallKit
+import Foundation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+
